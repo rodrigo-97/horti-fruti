ce5ce394e835c846c79d766b8e74ef8838e5a364
Rodrigo <rodmatheus99@gmail.com>
2022-07-07 09:28:58 -0300

    chore: add new migrations

--- database/migrations/1657119646861_users.ts
+++ database/migrations/1657119646861_users.ts
@@ -6,13 +6,11 @@ export default class UsersSchema extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.string('email', 255).notNullable()
+      table.string('email', 255).notNullable().unique()
       table.string('password', 180).notNullable()
       table.string('remember_me_token').nullable()
-
-      /**
-       * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
-       */
+      table.string('type', 35).notNullable()
+      table.timestamp('expires_at', { useTz: true })
       table.timestamp('created_at', { useTz: true }).notNullable()
       table.timestamp('updated_at', { useTz: true }).notNullable()
     })
